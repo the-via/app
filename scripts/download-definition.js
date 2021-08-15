e0acb49c825f3b3c031b71937a23afd29526e8c2
Olivia Briggs <ofbriggs@gmail.com>
2021-08-15 02:52:35 -0400

    Hack around handling keyboards repo release syncing temporarily

--- scripts/download-definition.js
+++ scripts/download-definition.js
@@ -3,10 +3,24 @@ const fs = require('fs');
 let url = 'https://www.caniusevia.com/keyboards.v2.json';
 
 let settings = {method: 'Get'};
+if (!fs.existsSync('public/definitions')) {
+  fs.mkdirSync('public/definitions');
+}
 
 fetch(url, settings)
   .then((res) => res.json())
   .then((json) => {
-    fs.writeFileSync('./public/keyboards.v2.json', JSON.stringify(json));
+    const {definitions, ...rest} = json;
+    const supportedKbs = Object.keys(definitions).map((i) => parseInt(i));
+    fs.writeFileSync(
+      './public/definitions/supported_kbs.json',
+      JSON.stringify({...rest, supportedKbs}),
+    );
+    Object.values(definitions).forEach((val) => {
+      fs.writeFileSync(
+        `./public/definitions/${val.vendorProductId}.json`,
+        JSON.stringify(val),
+      );
+    });
     // do something with JSON
   });
