a90aeac70d74969fe1980c6d100d07bad4a0b31a
Olivia Briggs <ofbriggs@gmail.com>
2022-12-30 20:30:02 -0500

    Clean up helpers

--- src/components/three-fiber/keyboard-canvas.tsx
+++ src/components/three-fiber/keyboard-canvas.tsx
@@ -119,13 +119,3 @@ export const KeyboardCanvasContent: React.VFC<{
     </PresentationControls>
   );
 }, shallowEqual);
-
-const debugShallowEqual = (obj: any, obj2: any) => {
-  return !Object.keys(obj).some((k) => {
-    const comparison = obj[k] !== obj2[k];
-    if (obj[k] !== obj2[k]) {
-      console.log(k, obj[k], obj2[k]);
-    }
-    return comparison;
-  });
-};
