379f32d2b114a959b86379699f36189cb393cf77
Olivia Briggs <ofbriggs@gmail.com>
2023-01-03 02:55:33 -0500

    Checkpoint progress for new case

--- src/components/three-fiber/case.tsx
+++ src/components/three-fiber/case.tsx
@@ -3,23 +3,14 @@ import React from 'react';
 import {useMemo} from 'react';
 import {shallowEqual} from 'react-redux';
 import {
-  getBasicKeyToByte,
   getSelectedDefinition,
   getSelectedKeyDefinitions,
 } from 'src/store/definitionsSlice';
 import {useAppSelector} from 'src/store/hooks';
 import {getSelectedTheme} from 'src/store/settingsSlice';
 import {getDarkenedColor} from 'src/utils/color-math';
-import {
-  calculateKeyboardFrameDimensions,
-  calculatePointPosition,
-  CSSVarObject,
-  getColors,
-  getKeyId,
-  getTextureColors,
-  KeycapMetric,
-} from 'src/utils/keyboard-rendering';
-import {Shape, Euler, Path} from 'three';
+import {KeycapMetric} from 'src/utils/keyboard-rendering';
+import {Shape, Path} from 'three';
 
 function makePlateShape(
   {width, height}: {width: number; height: number},
@@ -213,80 +204,6 @@ const SimplePlate: React.FC<{width: number; height: number}> = ({
     </group>
   );
 };
-const ComplexPlate = () => {
-  const depthOffset = 0.5;
-  const matrixKeycodes = [];
-  const keys: (VIAKey & {ei?: number})[] = useAppSelector(
-    getSelectedKeyDefinitions,
-  );
-  const definition = useAppSelector(getSelectedDefinition);
-  const {basicKeyToByte, byteToKey} = useAppSelector(getBasicKeyToByte);
-  const macros = useAppSelector((state) => state.macros);
-  if (!definition) {
-    return null;
-  }
-  const {width, height} = calculateKeyboardFrameDimensions(keys);
-  const keysKeys = useMemo(() => {
-    return {
-      indices: keys.map(
-        (k, i) => `${definition.vendorProductId}-${i}-${getKeyId(k)}`,
-      ),
-      coords: keys.map((k, i) => {
-        // x & y are pixel positioned
-        const [x, y] = calculatePointPosition(k);
-        const r = (k.r * (2 * Math.PI)) / 360;
-        // The 1.05mm in-between keycaps but normalized by a keycap width/height
-        const normalizedKeyXSpacing =
-          KeycapMetric.keyXSpacing / KeycapMetric.keyWidth;
-        const normalizedKeyYSpacing =
-          KeycapMetric.keyYSpacing / KeycapMetric.keyHeight;
-        const normalizedWidth = k.w + (k.w - 1) * normalizedKeyXSpacing;
-        const normalizedHeight = k.h + (k.h - 1) * normalizedKeyYSpacing;
-        return {
-          position: [
-            (KeycapMetric.keyXPos * x) / CSSVarObject.keyXPos,
-            (-y * KeycapMetric.keyYPos) / CSSVarObject.keyYPos,
-            0,
-          ],
-          rotation: [0, 0, -r],
-          scale: [normalizedWidth, normalizedHeight, 1],
-          color: getTextureColors(k),
-        };
-      }),
-    };
-  }, [definition, keys]);
-  const plateShape = makePlateShape({width, height}, keysKeys.coords);
-  const innerColor = '#212020';
-
-  return (
-    <group
-      position={[0.6, -depthOffset / 4, width / 2 + depthOffset / 2]}
-      rotation-z={(-7.5 * Math.PI) / 180}
-    >
-      <mesh rotation-y={Math.PI / 2} castShadow={true}>
-        <extrudeGeometry
-          attach="geometry"
-          args={[
-            plateShape,
-            {
-              bevelEnabled: true,
-              bevelSize: 0.1,
-              bevelThickness: 0.1,
-              bevelSegments: 10,
-              depth: 0.25,
-            },
-          ]}
-        />
-        <meshPhongMaterial
-          color={innerColor}
-          shininess={100}
-          reflectivity={1}
-          specular={'#161212'}
-        />
-      </mesh>
-    </group>
-  );
-};
 
 const Heart = React.memo(
   (props: {
@@ -321,8 +238,6 @@ const Heart = React.memo(
           props.caseHeight / 2,
           props.caseWidth / 2,
         ]}
-        scale={0.01}
-        rotation={[Math.PI / 2, 0, Math.PI / 2]}
       >
         <extrudeGeometry
           attach="geometry"
@@ -335,7 +250,135 @@ const Heart = React.memo(
   shallowEqual,
 );
 
+const makeShape2 = (layoutHeight: number) => {
+  const offsetXMultiplier = Math.tan((Math.PI * 7.5) / 180);
+  const bottomWidth = 10;
+  const topWidth = bottomWidth + offsetXMultiplier * layoutHeight;
+  const halfY = layoutHeight / 2;
+  // x is z
+  const path = new Shape();
+  let radius = 2;
+
+  let baseAngle = Math.PI / 2;
+  let inclineAngle = (Math.PI * 7.5) / 180;
+
+  path.moveTo(-topWidth, halfY);
+  path.absarc(
+    -topWidth - radius,
+    halfY - radius,
+    radius,
+    baseAngle * 1 + baseAngle,
+    baseAngle * 1,
+    true,
+  );
+  path.absarc(
+    -radius,
+    halfY,
+    radius,
+    baseAngle * 1,
+    baseAngle * 1 - baseAngle,
+    true,
+  );
+  path.absarc(
+    -radius,
+    -halfY,
+    radius,
+    baseAngle * 3 + baseAngle,
+    baseAngle * 3,
+    true,
+  );
+  path.absarc(
+    -bottomWidth - radius,
+    -halfY - radius,
+    radius,
+    baseAngle * 2 + baseAngle,
+    baseAngle * 2,
+    true,
+  );
+
+  //path.moveTo(-topWidth, halfY);
+  //path.lineTo(0, halfY);
+  //path.lineTo(0, -halfY);
+  //path.lineTo(-bottomWidth, -halfY);
+
+  return path;
+};
+
 export const Case = React.memo((props: {width: number; height: number}) => {
+  const theme = useAppSelector(getSelectedTheme);
+  const outsideColor = useMemo(() => theme[KeyColorType.Accent].c, [theme]);
+  const innerColor = '#212020';
+  const heartColor = useMemo(() => theme[KeyColorType.Accent].t, [theme]);
+  const properWidth =
+    props.width * KeycapMetric.keyXPos - KeycapMetric.keyXSpacing;
+  const properHeight =
+    KeycapMetric.keyYPos * props.height - KeycapMetric.keyYSpacing;
+  const insideBorder = 4;
+  const insideCaseThickness = properWidth + insideBorder * 1;
+  const outsideCaseThickness = properWidth + insideBorder * 2.5;
+  const shape = makeShape2(properHeight + insideBorder);
+  const outsideShape = makeShape2(properHeight + insideBorder * 2);
+  const bezelSize = 1;
+  const bevelSegments = 10;
+  // TODO: Figure out heart positioning
+  return (
+    <group scale={1} position-z={-bezelSize * 2} rotation-y={-Math.PI / 2}>
+      <Heart
+        caseWidth={properWidth}
+        caseHeight={properHeight}
+        caseThickness={0}
+        color={heartColor}
+      />
+      <mesh
+        position={[-bezelSize, 0, -outsideCaseThickness / 2]}
+        castShadow={true}
+      >
+        <extrudeGeometry
+          attach="geometry"
+          args={[
+            outsideShape,
+            {
+              depth: outsideCaseThickness,
+              bevelEnabled: true,
+              bevelSize: bezelSize,
+              bevelThickness: bezelSize,
+              bevelSegments,
+            },
+          ]}
+        />
+        <meshPhongMaterial
+          color={outsideColor}
+          shininess={100}
+          reflectivity={1}
+          specular={getDarkenedColor(outsideColor, 0.2)}
+        />
+      </mesh>
+      <mesh position={[0, 0, -insideCaseThickness / 2]} castShadow={true}>
+        <extrudeGeometry
+          attach="geometry"
+          args={[
+            shape,
+            {
+              depth: insideCaseThickness,
+              bevelEnabled: true,
+              bevelSize: bezelSize,
+              bevelThickness: bezelSize,
+              bevelSegments,
+            },
+          ]}
+        />
+        <meshPhongMaterial
+          color={innerColor}
+          shininess={100}
+          reflectivity={1}
+          specular={getDarkenedColor(outsideColor, 0.2)}
+        />
+      </mesh>
+    </group>
+  );
+});
+
+export const OldCase = React.memo((props: {width: number; height: number}) => {
   const innerColor = '#212020';
   const widthOffset = 0.4;
   const heightOffset = 0.5;
@@ -358,66 +401,69 @@ export const Case = React.memo((props: {width: number; height: number}) => {
   }, [props.height]);
 
   return (
-    <group
-      position={[
-        (KeycapMetric.keyXPos * (props.width + depthOffset)) / 2,
-        heightOffset / 2,
-        (-1 - 0.1) * KeycapMetric.keyXPos,
-      ]}
-      scale={KeycapMetric.keyXPos}
-      rotation={new Euler(-(Math.PI * 7.5) / 180, -Math.PI / 2, 0)}
-    >
-      <Heart
-        caseWidth={props.width}
-        caseHeight={props.height + heightOffset / 2}
-        caseThickness={2 * widthOffset}
-        color={heartColor}
-      />
-      <mesh position={[0, -0.1, 0]} castShadow={true}>
-        <extrudeGeometry
-          attach="geometry"
-          args={[
-            outsideShape,
-            {
-              bevelEnabled: true,
-              bevelSize: 0.1,
-              bevelThickness: 0.1,
-              bevelSegments: 10,
-              depth: props.width + depthOffset,
-            },
-          ]}
+    <group rotation-y={Math.PI} position={[0, 0, 0]}>
+      <group
+        position={[
+          -(KeycapMetric.keyXPos * (props.width + depthOffset)) / 2,
+          heightOffset / 2,
+          (-1 - 0.1) * KeycapMetric.keyXPos + 10 + 15,
+        ]}
+        rotation-y={Math.PI / 2}
+        scale={KeycapMetric.keyXPos}
+      >
+        <axesHelper args={[10]} />
+        <Heart
+          caseWidth={props.width}
+          caseHeight={props.height + heightOffset / 2}
+          caseThickness={2 * widthOffset}
+          color={heartColor}
         />
-        <meshPhongMaterial
-          color={outsideColor}
-          shininess={100}
-          reflectivity={1}
-          specular={getDarkenedColor(outsideColor, 0.2)}
-        />
-      </mesh>
-      {false ? (
-        <SimplePlate width={props.width} height={props.height} />
-      ) : (
-        <mesh position={[0.3, -0.1, depthOffset / 4]} castShadow={true}>
+        <mesh position={[0, 0, 0]} castShadow={true}>
           <extrudeGeometry
             attach="geometry"
             args={[
-              innerShape,
+              outsideShape,
               {
                 bevelEnabled: true,
-                bevelSize: 0.05,
-                bevelThickness: 0.05,
-                depth: props.width + depthOffset / 2,
+                bevelSize: 0.1,
+                bevelThickness: 0.1,
+                bevelSegments: 10,
+                depth: props.width + depthOffset,
               },
             ]}
           />
           <meshPhongMaterial
-            color={innerColor}
+            color={outsideColor}
             shininess={100}
             reflectivity={1}
             specular={getDarkenedColor(outsideColor, 0.2)}
           />
         </mesh>
-      )}
+        {false ? (
+          <SimplePlate width={props.width} height={props.height} />
+        ) : (
+          <mesh position={[0.3, -0.1, depthOffset / 4]} castShadow={true}>
+            <extrudeGeometry
+              attach="geometry"
+              args={[
+                innerShape,
+                {
+                  bevelEnabled: true,
+                  bevelSize: 0.05,
+                  bevelThickness: 0.05,
+                  depth: props.width + depthOffset / 2,
+                },
+              ]}
+            />
+            <meshPhongMaterial
+              color={innerColor}
+              shininess={100}
+              reflectivity={1}
+              specular={getDarkenedColor(outsideColor, 0.2)}
+            />
+          </mesh>
+        )}
+      </group>
     </group>
   );
 }, shallowEqual);
