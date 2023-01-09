7016aed4154f9bd2df54183b699ad91e8353922b
Olivia Briggs <ofbriggs@gmail.com>
2023-01-08 19:38:08 -0500

    Add new types to express different stages of sequencing

---
+++ src/utils/macro-api/types.ts
@@ -0,0 +1,28 @@
+export enum RawKeycodeSequenceAction {
+  Tap = 1, // \x01
+  Down = 2, // \x02
+  Up = 3, // \x03
+  Delay = 4, // \x04
+  Character = 42, // This is not a real QMK tag, it is implied from the absence of a tag
+}
+
+export enum GroupedKeycodeSequenceAction {
+  Chord = 5,
+  CharacterStream = 6,
+}
+
+export type RawKeycodeSequenceItem = [
+  RawKeycodeSequenceAction,
+  string | number,
+];
+
+export type GroupedKeycodeSequenceItem = [
+  GroupedKeycodeSequenceAction,
+  string | number,
+];
+
+export type RawKeycodeSequence = RawKeycodeSequenceItem[];
+export type OptimizedKeycodeSequence = (
+  | RawKeycodeSequenceItem
+  | RawKeycodeSequence
+)[];
