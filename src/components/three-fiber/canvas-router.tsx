f4d668d4d465d03e6305751ff487832c9038b512
Olivia Briggs <ofbriggs@gmail.com>
2023-01-28 02:00:55 -0500

    Headbang

--- src/components/three-fiber/canvas-router.tsx
+++ src/components/three-fiber/canvas-router.tsx
@@ -13,6 +13,7 @@ import {
   Float,
   Html,
   OrbitControls,
+  PresentationControls,
   SpotLight,
   useGLTF,
   useProgress,
@@ -86,13 +87,20 @@ const LoaderCubey: React.FC<{color: string; visible: boolean}> = React.memo(
 
     return (
       <>
-        <group
-          ref={spinnerRef}
-          rotation-y={Math.PI}
-          scale={0.8}
-          position={[0, yInit, -19]}
-        >
-          <primitive object={cubeyGLTF.scene} />
+        <group ref={spinnerRef} scale={0.8} position={[0, yInit, -19]}>
+          <PresentationControls
+            enabled={true} // the controls can be disabled by setting this to false
+            global={true} // Spin globally or by dragging the model
+            snap={true} // Snap-back to center (can also be a spring config)
+            speed={1} // Speed factor
+            zoom={1} // Zoom factor when half the polar-max is reached
+            rotation={[0, 0, 0]} // Default rotation
+            polar={[-Math.PI / 4, Math.PI / 4]} // Vertical limits
+            azimuth={[-Math.PI / 4, Math.PI / 4]} // Horizontal limits
+            config={{mass: 1, tension: 170, friction: 26}} // Spring config
+          >
+            <primitive object={cubeyGLTF.scene} />
+          </PresentationControls>
         </group>
       </>
     );
