ece380a8f50b5b48679826ab78a792b4d2e079a1
wilba <wilba@wilba.tech>
2023-01-28 15:55:51 +1100

    Handle deprecated keycodes in layout loading

---
+++ src/utils/key-to-byte/deprecated-keycodes.ts
@@ -0,0 +1,18 @@
+export default {
+  USER00: 'CUSTOM(0)',
+  USER01: 'CUSTOM(1)',
+  USER02: 'CUSTOM(2)',
+  USER03: 'CUSTOM(3)',
+  USER04: 'CUSTOM(4)',
+  USER05: 'CUSTOM(5)',
+  USER06: 'CUSTOM(6)',
+  USER07: 'CUSTOM(7)',
+  USER08: 'CUSTOM(8)',
+  USER09: 'CUSTOM(9)',
+  USER10: 'CUSTOM(10)',
+  USER11: 'CUSTOM(11)',
+  USER12: 'CUSTOM(12)',
+  USER13: 'CUSTOM(13)',
+  USER14: 'CUSTOM(14)',
+  USER15: 'CUSTOM(15)',
+} as Record<string, string>;
