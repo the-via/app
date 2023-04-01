f73868931ade971953a880a5bb17cd09615143c6
regs01 <megaone@yandex.ru>
2023-04-01 15:49:57 +0300

    Added more themes
    Added more Keychron themes and popular custom keycaps on Ali - Keychron Carbon Black B, Keychron Silver Grey A, Keychron Silver Grey B, Keychron Navy Blue A, Keychron Navy Blue B, Fendai, Honey Milk, Matcha, Black Grey, White Green, White, Black, Black and White, Classic Grey

--- src/utils/themes.ts
+++ src/utils/themes.ts
@@ -81,7 +81,7 @@ export const THEMES = {
     },
     CARBON_BLACK_A: {
       alpha: {
-        c: '#8c93a3',
+        c: '#788194',
         t: '#f3f3f3',
       },
       mod: {
@@ -92,7 +92,203 @@ export const THEMES = {
         c: '#e66b67',
         t: '#f3f3f3',
       },
-    },    
+    },
+    CARBON_BLACK_B: {
+      alpha: {
+        c: '#3b3b3e',
+        t: '#f3f3f3',
+      },
+      mod: {
+        c: '#788194',
+        t: '#f3f3f3',
+      },
+      accent: {
+        c: '#e66b67',
+        t: '#f3f3f3',
+      },
+    },
+    SILVER_GREY_A: {
+      alpha: {
+        c: '#b2b3b8',
+        t: '#f3f3f3',
+      },
+      mod: {
+        c: '#838589',
+        t: '#f3f3f3',
+      },
+      accent: {
+        c: '#e8db5d',
+        t: '#3d3125',
+      },
+    },
+    SILVER_GREY_B: {
+      alpha: {
+        c: '#838589',
+        t: '#f3f3f3',
+      },
+      mod: {
+        c: '#b2b3b8',
+        t: '#f3f3f3',
+      },
+      accent: {
+        c: '#e8db5d',
+        t: '#3d3125',
+      },
+    },
+    NAVY_BLUE_A: {
+      alpha: {
+        c: '#547be8',
+        t: '#f3f3f3',
+      },
+      mod: {
+        c: '#49599f',
+        t: '#f3f3f3',
+      },
+      accent: {
+        c: '#4dcfe0',
+        t: '#f3f3f3',
+      },
+    },
+    NAVY_BLUE_B: {
+      alpha: {
+        c: '#49599f',
+        t: '#f3f3f3',
+      },
+      mod: {
+        c: '#547be8',
+        t: '#f3f3f3',
+      },
+      accent: {
+        c: '#4dcfe0',
+        t: '#f3f3f3',
+      },
+    },
+    FENDAI: {
+      alpha: {
+        c: '#f0ebec',
+        t: '#07010f',
+      },
+      mod: {
+        c: '#f0ebec',
+        t: '#56395c',
+      },
+      accent: {
+        c: '#fc5d75',
+        t: '#56395c',
+      },
+    },
+    HONEY_MILK: {
+      alpha: {
+        c: '#fffff8',
+        t: '#07010f',
+      },
+      mod: {
+        c: '#fffff8',
+        t: '#07010f',
+      },
+      accent: {
+        c: '#f8b140',
+        t: '#07010f',
+      },
+    },
+    MATCHA: {
+      alpha: {
+        c: '#e8e8df',
+        t: '#4e475c',
+      },
+      mod: {
+        c: '#828572',
+        t: '#4e475c',
+      },
+      accent: {
+        c: '#828572',
+        t: '#4e475c',
+      },
+    },
+    BLACK_GREY: {
+      alpha: {
+        c: '#7e8293',
+        t: '#a5cbe6',
+      },
+      mod: {
+        c: '#364352',
+        t: '#eff3f2',
+      },
+      accent: {
+        c: '#364352',
+        t: '#eff3f2',
+      },
+    },
+    WHITE_GREEN: {
+      alpha: {
+        c: '#fefefe',
+        t: '#272727',
+      },
+      mod: {
+        c: '#275c65',
+        t: '#d0eae8',
+      },
+      accent: {
+        c: '#275c65',
+        t: '#d0eae8',
+      },
+    },
+    WHITE: {
+      alpha: {
+        c: '#fefefe',
+        t: '#272727',
+      },
+      mod: {
+        c: '#fefefe',
+        t: '#272727',
+      },
+      accent: {
+        c: '#fefefe',
+        t: '#272727',
+      },
+    },
+    BLACK: {
+      alpha: {
+        c: '#272727',
+        t: '#fefefe',
+      },
+      mod: {
+        c: '#272727',
+        t: '#fefefe',
+      },
+      accent: {
+        c: '#272727',
+        t: '#fefefe',
+      },
+    },
+    BLACK_AND_WHITE: {
+      alpha: {
+        c: '#fefefe',
+        t: '#272727',
+      },
+      mod: {
+        c: '#272727',
+        t: '#fefefe',
+      },
+      accent: {
+        c: '#272727',
+        t: '#fefefe',
+      },
+    },
+    CLASSIC_GREY: {
+      alpha: {
+        c: '#fcfcfc',
+        t: '#1a1a1a',
+      },
+      mod: {
+        c: '#a0a0a0',
+        t: '#1a1a1a',
+      },
+      accent: {
+        c: '#a0a0a0',
+        t: '#1a1a1a',
+      },
+    },
   },
   ...REMOTETHEMES,
 };
