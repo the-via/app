99a68a73076273c5730cedf62dd69225a6079797
Cipulot <40441626+Cipulot@users.noreply.github.com>
2023-11-27 23:03:50 +0100

    Update accent-range.tsx
    Adds explicit value view of the current slider value. The value updates on the fly when the slider is moving.

--- src/components/inputs/accent-range.tsx
+++ src/components/inputs/accent-range.tsx
@@ -1,28 +1,48 @@
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import styled from 'styled-components';
 
 const Container = styled.span`
-  display: inline-block;
+  display: flex;
+  align-items: center;
   line-height: initial;
   width: 200px;
 `;
 
-const SliderInput = styled.input.attrs({type: 'range'})<any>`
+const SliderInput = styled.input.attrs({type: 'range'})`
   accent-color: var(--color_accent);
   width: 100%;
+`;
+
+const ValueDisplay = styled.span`
+  margin-right: 10px;
 `;
 
 export const AccentRange: React.FC<
   Omit<React.InputHTMLAttributes<HTMLInputElement>, 'onChange'> & {
     onChange: (x: number) => void;
   }
-> = (props) => (
-  <Container>
-    <SliderInput
-      {...props}
-      onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-        props.onChange && props.onChange(+e.target.value);
-      }}
-    />
-  </Container>
-);
+> = (props) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+  const [sliderValue, setSliderValue] = useState(() => +inputRef.current?.value || 0);
+
+  useEffect(() => {
+    if (inputRef.current) {
+      setSliderValue(+inputRef.current.value);
+    }
+  }, [props.value]);
+
+  return (
+    <Container>
+      <ValueDisplay>{sliderValue}</ValueDisplay>
+      <SliderInput
+        ref={inputRef}
+        {...props}
+        onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+          const value = +e.target.value;
+          setSliderValue(value);
+          props.onChange?.(value);
+        }}
+      />
+    </Container>
+  );
+};
