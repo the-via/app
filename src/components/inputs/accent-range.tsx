6dac28354c1ff6b0bd6cd8dfc47dcf5051d3af66
Rishikesh Anand <rishi1998@gmail.com>
2024-10-16 16:18:56 -0700

    fix: Add a numeric text field to slider input for fine tune adjustments

--- src/components/inputs/accent-range.tsx
+++ src/components/inputs/accent-range.tsx
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styled from 'styled-components';
+import {NumberInput} from 'src/components/panes/configure-panes/submenus/macros/keycode-sequence-components';
 
 const Container = styled.span`
-  display: inline-block;
+  display: flex;
+  gap: 10px;
+  align-items: space-between;
   line-height: initial;
   width: 200px;
 `;
@@ -16,13 +19,21 @@ export const AccentRange: React.FC<
   Omit<React.InputHTMLAttributes<HTMLInputElement>, 'onChange'> & {
     onChange: (x: number) => void;
   }
-> = (props) => (
-  <Container>
-    <SliderInput
-      {...props}
-      onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-        props.onChange && props.onChange(+e.target.value);
-      }}
-    />
-  </Container>
-);
+> = (props) => {
+  const [value, setValue] = useState<number>(
+    (props.defaultValue as number) || 0,
+  );
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const newValue = +e.target.value;
+    setValue(newValue);
+    props.onChange && props.onChange(newValue);
+  };
+
+  return (
+    <Container>
+      <SliderInput {...props} value={value} onChange={handleChange} />
+      <NumberInput {...props} value={value} onChange={handleChange} />
+    </Container>
+  );
+};
