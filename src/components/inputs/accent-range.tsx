ee5e74f53e2ea80079041e04c0a9f1b7a8ca17c2
AkariiinMKII <6019344+AkariiinMKII@users.noreply.github.com>
2025-09-28 13:13:53 +0800

    Display value for slider

--- src/components/inputs/accent-range.tsx
+++ src/components/inputs/accent-range.tsx
@@ -1,28 +1,51 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 
 const Container = styled.span`
-  display: inline-block;
+  display: inline-flex;
+  align-items: center;
   line-height: initial;
-  width: 200px;
+  gap: 10px;
 `;
 
 const SliderInput = styled.input.attrs({type: 'range'})<any>`
   accent-color: var(--color_accent);
-  width: 100%;
+  width: 200px;
+  flex: none;
+`;
+
+const ValueDisplay = styled.span`
+  text-align: right;
+  font-size: 20px;
+  color: var(--color_label_highlighted);
+  white-space: nowrap;
 `;
 
 export const AccentRange: React.FC<
   Omit<React.InputHTMLAttributes<HTMLInputElement>, 'onChange'> & {
     onChange: (x: number) => void;
   }
-> = (props) => (
-  <Container>
-    <SliderInput
-      {...props}
-      onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-        props.onChange && props.onChange(+e.target.value);
-      }}
-    />
-  </Container>
-);
+> = (props) => {
+  const [currentValue, setCurrentValue] = useState<number>(
+    Number(props.defaultValue || props.value || props.min || 0)
+  );
+
+  useEffect(() => {
+    const newValue = Number(props.defaultValue || props.value || props.min || 0);
+    setCurrentValue(newValue);
+  }, [props.defaultValue, props.value, props.min]);
+
+  return (
+    <Container>
+      <ValueDisplay>{currentValue}</ValueDisplay>
+      <SliderInput
+        {...props}
+        onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+          const newValue = +e.target.value;
+          setCurrentValue(newValue);
+          props.onChange && props.onChange(newValue);
+        }}
+      />
+    </Container>
+  );
+};
